e7e953a9c465ba0e744ad01d1c189daa811c1510
faiq <ibrahimfaiq96@ymail.com>
2021-05-22 02:04:18 +0100

    added panic

--- main.py
+++ main.py
@@ -18,6 +18,7 @@ class Patitient(BaseModel):
     device_id: str
     status: str
 
+
 class Doctor(BaseModel):
     name: str
     _id: str
@@ -29,20 +30,26 @@ class Admin(BaseModel):
     _id: str
     status: str
 
+
 class Fall(BaseModel):
     time: str
+    date: str
     device_id: str
-    latlong: str
+    latitude: str
+    longitude: str
     status: str
+
 
 class Panic(BaseModel):
     time: str
+    date: str
     device_id: str
+    latitude: str
+    longitude: str
     status: str
 
 
 app = FastAPI()
-
 
 Database.initialize()
 
@@ -54,16 +61,17 @@ async def index():
 
 @app.get('/patients')
 async def show_patients():
-    #return db
+    # return db
     list_patients = []
     patients = Database.find("patient", {})
     for i in patients:
         list_patients.append(i)
     return list_patients
 
+
 @app.get('/patients/{patient_id}')
 async def show_patients_history(patient_id: str):
-    #return db
+    # return db
     list_patients = []
     patient = Database.find_one("patient", {"_id": patient_id})
     patients = Database.find("fall", {"device_id": patient['device_id']})
@@ -71,35 +79,68 @@ async def show_patients_history(patient_id: str):
         list_patients.append(i)
     return list_patients
 
+
 @app.post('/patients')
 async def register_patient(patient: Patitient):
     new_patient = patient.dict()
     new_patient['_id'] = uuid.uuid4().hex
     print(new_patient)
-    #db.append(new_pizza)
+    # db.append(new_pizza)
     Database.insert("patient", new_patient)
     return new_patient
 
+
 @app.get('/fall')
 async def show_falls():
-    #return db
+    # return db
     list_falls = []
     falls = Database.find("fall", {})
     for i in falls:
         list_falls.append(i)
     return list_falls
 
+
 @app.post('/fall')
 async def register_fall(fall: Fall):
     new_fall = fall.dict()
     new_fall['_id'] = uuid.uuid4().hex
     print(new_fall)
-    #db.append(new_pizza)
+    # db.append(new_pizza)
     Database.insert("fall", new_fall)
     return new_fall
 
-@app.get('/fall/{device_id}/{time}/{latlong}/{status}')
-async def register_fall_get(device_id: str, time: str, latlong: str, status: str):
+
+@app.get('/fall/{device_id}/{time}/{date}/{latitude}/{longitude}/{status}')
+async def register_fall_get(device_id: str, time: str, date: str, latitude: str, longitude: str, status: str):
     _id = uuid.uuid4().hex
-    Database.insert("fall", {"_id": _id, "device_id": device_id, "time": time, "latlong": latlong, "status": status})
+    Database.insert("fall", {"_id": _id, "device_id": device_id, "time": time, "date": date, "latitude": latitude,
+                             "longitude": longitude, "status": status})
+    return "success"
+
+
+@app.get('/panic')
+async def show_panics():
+    # return db
+    list_panics = []
+    panics = Database.find("panic", {})
+    for i in panics:
+        list_panics.append(i)
+    return list_panics
+
+
+@app.post('/panic')
+async def register_panic(panic: Panic):
+    new_panic = panic.dict()
+    new_panic['_id'] = uuid.uuid4().hex
+    print(new_panic)
+    # db.append(new_pizza)
+    Database.insert("panic", new_panic)
+    return new_panic
+
+
+@app.get('/panic/{device_id}/{time}/{date}/{latitude}/{longitude}/{status}')
+async def register_panic_get(device_id: str, time: str, date: str, latitude: str, longitude: str, status: str):
+    _id = uuid.uuid4().hex
+    Database.insert("panic", {"_id": _id, "device_id": device_id, "time": time, "date": date, "latitude": latitude,
+                             "longitude": longitude, "status": status})
     return "success"
